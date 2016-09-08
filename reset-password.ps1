1c774713e332e6310a15f53822b152b377398804
Jeff Buenting <jeffbuenting@users.noreply.github.com>
2016-09-07 21:04:34 -0400

    uploads

---
+++ reset-password.ps1
@@ -0,0 +1,13 @@
+﻿$OU = 'ou=Human Services,ou=users,ou=managed,dc=vbgov,dc=com'
+
+$users = Import-Csv c:\temp\data.csv 
+
+foreach ($U in $Users ) {
+        
+		
+		    set-QADUser -Identity $U.uname -userpassword 'P@ssword'       ## test existance of user
+        	
+		    Enable-QADUser $U.uname
+		
+}
+
